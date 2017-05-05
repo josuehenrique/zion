4d37b881e668f442659aff2360a910d1955f6b9c
josuehenrique <josue_mses@hotmail.com>
2017-05-05 10:30:20 -0300

    Remove campo cpf da factory de user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     avatar { File.new("#{Rails.root}/spec/fixtures/images/test.jpg") }
     email 'teste@hotmail.com'
     password '124356'
-    cpf '76727744335'
     locked false
     admin true
     name 'Josué'
@@ -12,7 +11,6 @@ FactoryGirl.define do
     factory :user_peter do
       name 'Pedro Augusto'
       email 'peter@gmail.com'
-      cpf '00461262100'
       admin false
     end
   end
