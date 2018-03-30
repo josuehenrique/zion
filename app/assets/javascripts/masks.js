b48bb23280e8620f4c7f489392dc7b21e675bfc0
Josué Henrique Ferreira da Silva <josuehenriqueferreira@gmail.com>
2018-03-29 23:27:16 -0300

    Ajusta formulário de membro

--- app/assets/javascripts/masks.js
+++ app/assets/javascripts/masks.js
@@ -1,11 +1,13 @@
 function addMasks() {
   $('.mask-cpf').mask("999.999.999-99");
+  $('.mask-zipcode').mask("99.999-999");
 }
 
 // Remove mask on submit
 function removeMasks() {
   $('form').submit(function () {
     $('.mask-cpf').mask("99999999999");
+    $('.mask-zipcode').mask("99999999");
   });
 }
 
