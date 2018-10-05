26556bce4727aca01d3b0ba4262217816b8b522a
josuehenrique <josuehenriqueferreira@gmail.com>
2018-10-05 02:43:52 -0300

    Add address to congregateds

--- app/models/congregated.rb
+++ app/models/congregated.rb
@@ -1,12 +1,15 @@
 class Congregated < ActiveRecord::Base
-  attr_accessible :name, :email, :birth_dt, :entry_dt, :baptized, :phone_attributes
+  attr_accessible :name, :email, :birth_dt, :entry_dt, :baptized,
+    :phone_attributes, :address_attributes
 
   attr_list :name, :entry_dt, :baptized
 
   attr_search :name
 
+  has_one :address, as: :related, dependent: :destroy
   has_one :phone, as: :related, dependent: :destroy
 
+  accepts_nested_attributes_for :address, update_only: true
   accepts_nested_attributes_for :phone, update_only: true
 
   validates :name, :birth_dt, :entry_dt, presence: true
