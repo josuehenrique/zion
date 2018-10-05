efd5985287d37410f8fcbc1fa68b3d822c006307
josuehenrique <josuehenriqueferreira@gmail.com>
2018-10-05 02:41:12 -0300

    Add phone to congregateds

--- app/models/congregated.rb
+++ app/models/congregated.rb
@@ -1,9 +1,13 @@
 class Congregated < ActiveRecord::Base
-  attr_accessible :name, :email, :birth_dt, :entry_dt, :baptized
+  attr_accessible :name, :email, :birth_dt, :entry_dt, :baptized, :phone_attributes
 
   attr_list :name, :entry_dt, :baptized
 
   attr_search :name
+
+  has_one :phone, as: :related, dependent: :destroy
+
+  accepts_nested_attributes_for :phone, update_only: true
 
   validates :name, :birth_dt, :entry_dt, presence: true
 
