33d801f43836dd0633db8d6e82b069e73a2146c0
josuehenrique <josuehenriqueferreira@gmail.com>
2018-10-05 02:39:50 -0300

    Create phone mask

--- app/assets/javascripts/masks.js
+++ app/assets/javascripts/masks.js
@@ -1,6 +1,7 @@
 function addMasks() {
   $('.mask-cpf').mask("999.999.999-99");
   $('.mask-zipcode').mask("99.999-999");
+  $('.mask-phone').mask("(99) 99999-9999");
 }
 
 // Remove mask on submit
@@ -8,6 +9,7 @@ function removeMasks() {
   $('form').submit(function () {
     $('.mask-cpf').mask("99999999999");
     $('.mask-zipcode').mask("99999999");
+    $('.mask-phone').mask("99999999999");
   });
 }
 
