221505a5ee3af253143daa42e2bf2613af213a8b
josuehenrique <josuehenriqueferreira@gmail.com>
2018-10-14 11:26:48 -0300

    Fix migration

--- db/migrate/20181005045331_add_phone_type.rb
+++ db/migrate/20181005045331_add_phone_type.rb
@@ -1,5 +1,5 @@
 class AddPhoneType < ActiveRecord::Migration
   def change
-    add_column :phones, :phone_type, :string, limit: 10, null: false, default: 'residential'
+    add_column :phones, :phone_type, :string, limit: 11, null: false, default: 'residential'
   end
 end
