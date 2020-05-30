372a638a4bc546408741ca84940891699855c9f9
josuehenriqueferreira <josue@neomed.tech>
2020-05-30 08:03:11 -0300

    Add code on inventories list

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,7 +1,7 @@
 class Inventory < ActiveRecord::Base
   attr_accessible :name, :description, :observation
 
-  attr_list :name
+  attr_list :id, :name
   attr_search :name
 
   validates :name, :description, presence: true
