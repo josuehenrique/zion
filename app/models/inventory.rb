9d0b39ec317941947715840f2e78ea49b706d0d3
Josué Henrique Ferreira da Silva <josue@neomed.tech>
2022-05-28 16:17:04 -0300

    Add annotations for inventory.rb

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: inventories
+#
+#  id           :bigint(8)        not null, primary key
+#  name         :string           not null
+#  description  :text             not null
+#  observations :text
+#  active       :boolean          default(TRUE), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Inventory < ActiveRecord::Base
   attr_accessible :name, :description, :observations
 
