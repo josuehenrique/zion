223684f9d4f499d3542180e74d85c29cf2bad79c
Josué Henrique Ferreira da Silva <josue@neomed.tech>
2022-05-29 01:15:26 -0300

    remove phone masks

--- app/assets/javascripts/masks.js
+++ app/assets/javascripts/masks.js
@@ -1,7 +1,6 @@
 function addMasks() {
   $('.mask-cpf').mask("999.999.999-99");
   $('.mask-zipcode').mask("99.999-999");
-  $('.mask-phone').mask("(99) 99999-9999");
 }
 
 // Remove mask on submit
@@ -9,7 +8,6 @@ function removeMasks() {
   $('form').submit(function () {
     $('.mask-cpf').mask("99999999999");
     $('.mask-zipcode').mask("99999999");
-    $('.mask-phone').mask("99999999999");
   });
 }
 
